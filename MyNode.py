50fb761961a2a1fbd590508554717381d0dddb59
aytekincem <mehmetaytekin@sabanciuniv.edu>
2018-07-18 23:11:53 +0300

    Faster queries. Driver updated. New delete operations added.

--- MyNode.py
+++ MyNode.py
@@ -3,8 +3,17 @@ from json import dumps
 from flask import Flask, g, Response, request, render_template
 from elasticsearch import Elasticsearch
 from neo4j.v1 import GraphDatabase, basic_auth
+import courses_get
 
-class Node(object):
+global DRIVER
+global db
+global es
+
+DRIVER=courses_get.driver
+db=DRIVER.session()
+es = Elasticsearch()
+class MyNode(object):
+
     id=0
     creator = ""
     label = ""
@@ -30,11 +39,12 @@ class Node(object):
         self.description=description
         self.alternatives=alternatives
 
+
     def tieToTopic(self, topic_name2):
 
-        driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+        #if not hasattr(g, 'neo4j_db'):
+            #db = driver.session()
         all_query="MATCH (n),(m) where n.title="+self.title+ "and m.title="+"'"+topic_name2+"'"+"CREATE (n) -[:prev]->(m) CREATE (m) -[:next]->(n)"
         print(all_query)
         results = db.run(all_query)
@@ -44,9 +54,9 @@ class Node(object):
         creator="'Veli Aytekin'"  #becareful to the surronding '   '
         link="'www.abc.com'"
         resource="'video'"
-        driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+       # if not hasattr(g, 'neo4j_db'):
+        #    db = DRIVER.session()
         all_query = "MATCH (n) where n.title="+self.title+"CREATE (m:alternative { creator:"+creator+", link:"+link+", resource:"+resource+"   } ) CREATE (n)-[:hasAlternative]->(m)"
         print(all_query)
         results = db.run(all_query)
@@ -56,9 +66,9 @@ class Node(object):
         creator="'Ali Aytekin'"
         link="'www.xyz.com'"
         type="'multiple-choice'"
-        driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+       # if not hasattr(g, 'neo4j_db'):
+            #db = DRIVER.session()
         all_query = "MATCH (n) where n.title=" + self.title + "CREATE (m:assesment { creator:" + creator + ", link:" + link + ", type:" + type + "   } ) CREATE (n)-[:hasAssesment]->(m)"
         print(all_query)
         results = db.run(all_query)
@@ -73,9 +83,10 @@ class Node(object):
 
 
 def deleteAlternative(title):
-        driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+
+       # if not hasattr(g, 'neo4j_db'):
+            #db = DRIVER.session()
         title="'"+title+"'"
 
         query1="match (n)- [r:hasAlternative] - (m) where n.title= "+title+ " delete r,m"  # UNION ALL"
@@ -86,8 +97,8 @@ def deleteAlternative(title):
 
 def deleteAssesment(title):
         driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #if not hasattr(g, 'neo4j_db'):
+           # db = driver.session()
         title = "'" + title + "'"
 
         query1 = "match (n)- [r:hasAssesment] - (m) where n.title= " + title + " delete r,m"  # UNION ALL"
@@ -96,9 +107,9 @@ def deleteAssesment(title):
         return "asessment deleted"
 
 def deleteNode(title):
-    driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-    if not hasattr(g, 'neo4j_db'):
-        db = driver.session()
+    #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+    #if not hasattr(g, 'neo4j_db'):
+        #db = DRIVER.session()
 
     deleteAlternative(title)
     deleteAssesment(title)
@@ -107,15 +118,15 @@ def deleteNode(title):
     query1 = "match (n)- [r] - () where n.title= " + title + " delete r,n"
     print(query1)
     results = db.run(query1)
-    es = Elasticsearch()
+    #es = Elasticsearch()
     es.delete_by_query(index='courses', doc_type='node',body={ 'query': { 'match': { 'title': title } } })
     return "node deleted"
 
 def make_node(creator,label,link,pos,resource,title,type,tags,description):
 
-        driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
-        if not hasattr(g, 'neo4j_db'):
-            db = driver.session()
+        #driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'))
+        #if not hasattr(g, 'neo4j_db'):
+            #db = driver.session()
         creator = "'" + creator + "'"
         label = "'" + label + "'"
         link = "'" + link + "'"
@@ -131,17 +142,59 @@ def make_node(creator,label,link,pos,resource,title,type,tags,description):
         for record in results:
             id = record['id(n)']
 
-        es = Elasticsearch()
+        #es = Elasticsearch()
 
         res = es.index(index='courses', doc_type='node', id=id,
                        body={'title': title, 'description': description, 'tags': tag_list})
 
-        node = Node(id,creator,label,link,pos,resource,title,type,tags,description)
+        node = MyNode(id,creator,label,link,pos,resource,title,type,tags,description)
 
-        if hasattr(g, 'neo4j_db'):
-                g.neo4j_db.close()
+        #if hasattr(g, 'neo4j_db'):
+                #g.neo4j_db.close()
+
 
         return node
 
+def getNodeInfo():
+    results = db.run("MATCH (n:course) where id(n)=1 RETURN n.label as label, n.title as title , n.creator as creator , n.link as link , n.pos as pos, n.resource as resource, n.type as type ")
+    nodes = []
+    for record in results:
+        label=record['label']
+        title=record['title']
+        creator = record['creator']
+        link= record['link']
+        pos= record['pos']
+        resource = record['resource']
+        type = record['type']
 
+    print(label," ",title," ",creator)
 
+    #es = Elasticsearch()
+    resp = es.search(index="courses", body={"query": {"match": {"_id": 1}}})
+    for doc in resp['hits']['hits']:
+        id = doc['_id']
+        tags = doc['_source']['tags']  # tags type list.
+        description = doc['_source']['description']
+
+    print(id," ",tags," ")
+
+    alternative_nodes = []
+    query = "MATCH (n:course)-[:hasAlternative]->(alternative) WHERE id(n)=1 RETURN alternative.creator as al_creator,id(alternative) as al_id, alternative.link as al_link, alternative.resource as al_resource "
+    results = db.run(query)
+    for record in results:
+        al_id=record['al_id']
+        al_creator = record['al_creator']
+        al_link = record['al_link']
+        al_resource = record['al_resource']
+        alternative_node=Alternative.make_alternative(al_id,al_creator,al_link,al_resource)
+        alternative_nodes.append(alternative_node)
+    print(al_creator)
+
+    return "ok"
+
+def GetLastNode():
+    query="match(a:course) where not (a)-[:next]->() return a.title as title"
+    results = db.run(query)
+    for record in results:
+        title=record['title']
+    return title
--- courses_get.py
+++ courses_get.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 import os
-import Node
+import MyNode
 import Alternative
 from json import dumps
 from flask import Flask, g, Response, request, render_template
 from elasticsearch import Elasticsearch
 from flask_cors import CORS
 from neo4j.v1 import GraphDatabase, basic_auth
-
+from py2neo import Graph,Node,Relationship
 app = Flask(__name__, static_url_path='/static/') #main web app objemiz.
 CORS(app)   #for cross domain.
 #password = os.getenv("NEO4J_PASSWORD")
 #change2
 
-driver = GraphDatabase.driver('bolt://localhost',auth=basic_auth("neo4j", 'cem'))
+global driver
+driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", 'cem'),encrypted=False,keep_alive=True)
 
-
+global es
+es=Elasticsearch()
 def getNext(current_topic):
     db = get_db()
     query="MATCH (n:course)-[:next]->(next_course) WHERE n.title='"+current_topic+"' RETURN collect(next_course.title) as returned_course "
@@ -94,61 +96,38 @@ def giveData():
 
 @app.route("/getNode",methods=['GET','POST'])
 
-def getNodeInfo():
-    db = get_db()
-    results = db.run("MATCH (n:course) where id(n)=1 RETURN n.label as label, n.title as title , n.creator as creator , n.link as link , n.pos as pos, n.resource as resource, n.type as type ")
-    nodes = []
-    for record in results:
-        label=record['label']
-        title=record['title']
-        creator = record['creator']
-        link= record['link']
-        pos= record['pos']
-        resource = record['resource']
-        type = record['type']
+def getNode():
 
-    print(label," ",title," ",creator)
+    return MyNode.getNodeInfo()
 
-    es = Elasticsearch()
-    resp = es.search(index="courses", body={"query": {"match": {"_id": 1}}})
-    for doc in resp['hits']['hits']:
-        id = doc['_id']
-        tags = doc['_source']['tags']  # tags type list.
-        description = doc['_source']['description']
+@app.route("/getPrev",methods=['GET','POST'])
 
-    print(id," ",tags," ")
+def getPrevious():
+    i=100
+    liste=[]
+    while(i>0):
+        i=i-1
+        getPrev("C")
+        liste.append("x")
 
-    alternative_nodes = []
-
-    db = get_db()
-    query = "MATCH (n:course)-[:hasAlternative]->(alternative) WHERE id(n)=1 RETURN alternative.creator as al_creator,id(alternative) as al_id, alternative.link as al_link, alternative.resource as al_resource "
-    results = db.run(query)
-    for record in results:
-        al_id=record['al_id']
-        al_creator = record['al_creator']
-        al_link = record['al_link']
-        al_resource = record['al_resource']
-        alternative_node=Alternative.make_alternative(al_id,al_creator,al_link,al_resource)
-        alternative_nodes.append(alternative_node)
-    print(al_creator)
-
-    return "ok"
+    return "OK"
 
 @app.route("/makeNode",methods=['GET','POST'])
 def makeNode():
-    node=Node.make_node("Eralp","CS300","www.yahoo.com","mid","video","B","topic",["a","b","c"],"This is course B")
-    if(node.getPosition()!="start"):
-        node.tieToTopic("A")
+    lastNode=""
+    lastNode = MyNode.GetLastNode()
+    node=MyNode.make_node("Ali", "CS300", "www.yahoo.com", "mid", "video", "D", "topic", ["a", "b", "c"], "This is course D")
+    node.tieToTopic(lastNode)
     node.addAlternative()
     node.addAssesment()
 
-    #node.addAlternative()
-
     return "ok"
+
+
 
 @app.route("/deleteNode",methods=['GET','POST'])
 def deleteNode():
-    Node.deleteNode("B")
+    MyNode.deleteNode("D")
     return "ok"
 
 
